93d2bad15dade19a41358fd46f4c6a49436e936e
Spatchy <spatchy@spatchy.net>
2023-09-21 10:54:44 +0100

    implement likes effect on timer

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,11 @@ const { google } = require("googleapis")
 
 async function main () {
 
-  const calculateRemainingTime = (viewsAtLastCheck, timerAtLastCheck, currentViews) => {
+  const calculateRemainingTime = (viewsAtLastCheck, likesAtLastCheck, timerAtLastCheck, currentViews, currentLikes) => {
     const newViews = currentViews - viewsAtLastCheck
+    const newLikes = currentLikes - likesAtLastCheck
     const tickedTimer = timerAtLastCheck - 600 // runs every 10 minutes so remove 10 minutes from timer
-    const finalTimer = tickedTimer + (newViews * 30)
+    const finalTimer = tickedTimer + (newViews * 30) + (newLikes * 60)
     if (finalTimer <= 0) {
       destroyVideo()
     }
@@ -52,13 +53,21 @@ async function main () {
   })
 
   // 48 hours = 172800
-  const timer = calculateRemainingTime(tagsObject.savedViews, tagsObject.savedTimer, statistics.viewCount)
+  const timer = calculateRemainingTime(
+    tagsObject.savedViews,
+    tagsObject.savedLikes,
+    tagsObject.savedTimer,
+    statistics.viewCount,
+    statistics.likeCount
+  )
+
   const stringifiedTimer = stringifyTimerToHours(timer)
 
   const newTitle = `This video will self destruct in ${stringifiedTimer} hours`
   const newTags = [
     `savedViews=${statistics.viewCount}`,
-    `savedTimer=${timer}`
+    `savedTimer=${timer}`,
+    `savedLikes=${statistics.likeCount}`
   ]
 
   console.log(newTitle)
